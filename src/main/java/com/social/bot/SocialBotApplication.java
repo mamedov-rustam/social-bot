3828499772fa1a374f4403c9ea8e49fac106adb5
rmamedov <mamedov.developer@gmail.com>
2017-06-07 16:06:22 +0300

    Improvement

--- src/main/java/com/social/bot/SocialBotApplication.java
+++ src/main/java/com/social/bot/SocialBotApplication.java
@@ -1,14 +1,11 @@
 package com.social.bot;
 
 import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-@EnableFeignClients
 @SpringBootApplication
 public class SocialBotApplication {
 
